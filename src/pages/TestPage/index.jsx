8692462f7d55a5730f51956fce2ac72978a4a626
wlsdnr710 <wlsdnr710@naver.com>
2025-11-19 12:12:20 +0900

    frontend: Router 설치 + 테스트용 임시 페이지 생성

---
+++ src/pages/TestPage/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+/**
+ * 재사용 가능한 공용 테스트 컴포넌트를 위한 임시 테스트 페이지
+ * 나중에 구현이 시작되면 삭제 or 다른 router 로 바꿔주세요.
+ * @returns 테스트 페이지
+ */
+export const Testpage = () => {
+  return (
+    <div>
+      <h1>This is TestPage</h1>
+      <p>컴포넌트 테스트 할 임시 테스트 페이지</p>
+    </div>
+  );
+};
