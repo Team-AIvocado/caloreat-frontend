6e467e5484a6cc48145a04ad9915cc9f87f9e370
yewon <yeily98@icloud.com>
2025-11-23 12:02:03 +0900

    userinfo first commit

--- src/pages/UserInfoPage/index.jsx
+++ src/pages/UserInfoPage/index.jsx
@@ -1,3 +1,18 @@
 export const UserInfoPage = () => {
-  return <></>;
+  return (
+    <>
+      <div className="flex h-screen flex-col justify-center items-center">
+        <div className="text-main_color text-3xl">
+          <div className=" pb-7 font-bold">caloreat</div>
+        </div>
+
+        <div className="flex flex-col">
+          <input
+            className="border bg-white my-2 focus:ring-1 focus:ring-main_color/50 focus:outline-none focus:border-main_color border-border_color text-sm pl-2 pr-11 py-3"
+            type="date"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
