5398abfb2d6650edabdde1d703d2edcd9ee0f7b8
yewon <yeily98@icloud.com>
2025-11-25 05:33:36 +0900

    axios connected

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { api } from "../../api/axios";
 
 export const LoginPage = () => {
   const [userId, setUserId] = useState("");
@@ -17,6 +18,15 @@ export const LoginPage = () => {
     navigate("/signup");
   };
 
+  const test = async () => {
+    try {
+      const response = await api.get("/");
+      console.log(response);
+    } catch (e) {
+      console.log("failed to fetch data", e);
+    }
+  };
+
   //TODO: 존재하지 않는 id라면 modal창 출력
   //TODO: 로그인한 사용자 정보 테이블에 사용자 아이디가 없다면 사전정보 입력 페이지로 이동
   // 사전 정보가 있다면 main으로 이동
@@ -30,7 +40,8 @@ export const LoginPage = () => {
       return;
     } else setError({ ...error, pw: false });
 
-    navigate("/userinfo");
+    test();
+    // navigate("/userinfo");
     // navigate("/main");
   };
 
