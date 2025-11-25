d7c886d21a78ab2b5676d369d650e91b0b79b143
yewon <yeily98@icloud.com>
2025-11-25 17:56:30 +0900

    userinfoMob first commit

---
+++ src/utils/styles.js
@@ -0,0 +1,10 @@
+//성별 스타일
+export const sty = [
+  "bg-white text-secondary_text text-xs px-10 py-2 rounded-lg mr-3 mb-3 border border-border_color",
+  "bg-white text-secondary_text text-xs px-10 py-2 rounded-lg mr-3 mb-3 border border-main_color",
+];
+
+export const sty1 = [
+  "bg-white text-secondary_text text-xs px-4 py-1 rounded-lg mr-2 border border-border_color",
+  "bg-white text-secondary_text text-xs px-4 py-1 rounded-lg mr-2 border border-main_color",
+];
