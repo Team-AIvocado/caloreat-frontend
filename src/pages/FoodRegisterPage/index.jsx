a3ec468b0e9663a71f9e3051d5c3168b72bf31f5
yewon <yeily98@icloud.com>
2025-12-04 16:28:35 +0900

    add food analysis component

--- src/pages/FoodRegisterPage/index.jsx
+++ src/pages/FoodRegisterPage/index.jsx
@@ -1,20 +1,34 @@
 import { useState } from "react";
 import { useAlert } from "../../context/AlertContext";
 import { ImageInput } from "./layout/ImageInput";
+import { ImageAnalysis } from "./layout/ImageAnalysis";
 
 export const FoodRegisterPage = () => {
   const { showAlert, closeAlert } = useAlert();
   const [cameraMode, setCameraMode] = useState(false);
   const [imgSrc, setImgSrc] = useState("");
+  const [analysisMode, setAnalysisMode] = useState(false);
 
   return (
-    <ImageInput
-      showAlert={showAlert}
-      closeAlert={closeAlert}
-      setCameraMode={setCameraMode}
-      setImgSrc={setImgSrc}
-      cameraMode={cameraMode}
-      imgSrc={imgSrc}
-    />
+    <div className="flex flex-col justify-center items-center">
+      <div className="pt-24 pb-11 text-center text-2xl text-secondary_text">
+        음식 기록하기
+      </div>
+      <div className="flex flex-col justify-center">
+        {analysisMode ? (
+          <ImageAnalysis />
+        ) : (
+          <ImageInput
+            showAlert={showAlert}
+            closeAlert={closeAlert}
+            setCameraMode={setCameraMode}
+            setImgSrc={setImgSrc}
+            cameraMode={cameraMode}
+            imgSrc={imgSrc}
+            setAnalysisMode={setAnalysisMode}
+          />
+        )}
+      </div>
+    </div>
   );
 };
---
+++ src/pages/FoodRegisterPage/layout/ImageAnalysis.jsx
@@ -0,0 +1,3 @@
+export const ImageAnalysis = () => {
+  return <div>image analysis 음식 인식 완료!</div>;
+};
