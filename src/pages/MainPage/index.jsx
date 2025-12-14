e2e72c05002c413dcefc4fac0461c1c9e248e50c
wlsdnr710 <wlsdnr710@naver.com>
2025-12-14 10:03:26 +0900

    배포 테스트 #4

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -9,9 +9,6 @@ export const MainPage = () => {
     <div>
       <MainNavBar nickname={userInfo?.nickname || user?.nickname || ""} />
       <div className="ml-52 pb-0">
-        <div className="fixed bottom-2 right-2 text-xs text-gray-400">
-          Deploy v1.1
-        </div>
         <Outlet />
       </div>
     </div>
