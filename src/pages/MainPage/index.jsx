98d3ab427830ac96f6028777b770bf8362bcbe15
wlsdnr710 <wlsdnr710@naver.com>
2025-12-14 09:51:49 +0900

    chore: canary version tag

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -9,6 +9,9 @@ export const MainPage = () => {
     <div>
       <MainNavBar nickname={userInfo?.nickname || user?.nickname || ""} />
       <div className="ml-52 pb-0">
+        <div className="fixed bottom-2 right-2 text-xs text-gray-400">
+          Deploy v1.1
+        </div>
         <Outlet />
       </div>
     </div>
